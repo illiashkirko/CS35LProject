4f3e8fc5d2d2b7ff171365addb59baac84530415
Aryaman <aryamankumar02@gmail.com>
2022-05-03 18:13:06 -0700

    Positioned & resized input elements, added tweet button (alerts tweet on submission).

--- blink/src/index.js
+++ blink/src/index.js
@@ -12,12 +12,30 @@ root.render(
 );
 
 class InputBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('You tweeted ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>
-          <input type="text" /> 
+          <textarea value={this.state.value} onChange={this.handleChange} />
         </label>
+        <input type="submit" value="Tweet" />
       </form>
     );
   }
