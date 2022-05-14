0a9ae6bdc025b29ab950ffcb69738198984f8b56
aryaman175 <64005489+aryaman175@users.noreply.github.com>
2022-05-14 15:21:45 -0700

    messages.js comments

--- blink/server/routes/messages.js
+++ blink/server/routes/messages.js
@@ -1,22 +1,27 @@
 const router = require('express').Router();
-let Messages= require('../models/messages.model')
+let Messages= require('../models/messages.model')   //connecting to messages model
 
+//first route, get requests
 router.route('/').get((reg, res) => {
-    Messages.find()
-    .then(messages => res.json(messages))
-    .catch(err => res.status(400).json('Error: ' + err));
+    Messages.find()                             //method that gets all messages from the database
+    .then(messages => res.json(messages))       //returns all the messages in JSON format
+    .cat(err => res.status(400).json('Error: ' + err));
 
 });
 
+//post requests, adding data to database
 router.route('/add').post((req,res) => {
+    //extracting data from request
     const userMessages = req.body.userMessages;
     const numberOfLikes = req.body.numberOfLikes;
 
+    //creating new message based on data
     const newMessage = new Messages({
         userMessages,
         numberOfLikes,
     });
 
+    //saving message on db
     newMessage.save()
     .then(()=> res.json('Message submitted'))
     .catch(err=> res.status(400).json('Error' + err));
