3ae23fe69fd37c24dc8438d978ae163a3fa2368c
mihirbaviskar <36090347+mihirbaviskar@users.noreply.github.com>
2022-05-25 17:06:56 -0700

    Add files via upload

--- blink/src/profile.js
+++ blink/src/profile.js
@@ -1,7 +1,45 @@
 import React from "react";
+import './profile.css'
 
 function Profile() {
-  return <div> THIS IS THE Profile PAGE</div>;
-}
+  let username = "Username";
+  let followerCount = "20";
+  let followingCount = "25";
+  let bio  = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum sem condimentum viverra pretium.";
+  return(
+    <>
+    <br></br>
+    <div>
+      <header id="profheader">
+      <table id="profdata">
+        <tr>
+          <td id = "profusercontainer" ><div id="divusername"><p id="profusername">{username}</p></div></td>
+          <td>        
+            <section id="profnums">
+              <table id="fdata">
+                <tr>
+                  <td class="fdataitem"> Followers </td>
+                  <td class="fdataitem"> Follwing </td>
+                </tr>
+                <tr>
+                  <td class="fdataitem"> {followerCount} </td>
+                  <td class="fdataitem"> {followingCount} </td>
+                </tr>
+              </table>
+            </section>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2" id="biodata"><div id="profbio">{bio}</div></td>
+        </tr>
+      </table>
+      </header>
+      <br></br>
+      <hr></hr>
+    </div>
+    </>
+  );
+ }
+ 
 
 export default Profile;
