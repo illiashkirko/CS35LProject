29d6a621dfb59a5bf0a84ccf9a3df444c647fbb7
liz <liyulg0@gmail.com>
2022-06-02 19:59:37 -0700

    Added images

--- blink/src/imageLinks.js
+++ blink/src/imageLinks.js
@@ -2,8 +2,8 @@ export const imageLinks=[
     "https://upload.wikimedia.org/wikipedia/commons/4/40/Portrait_of_a_father.jpg",
     "https://upload.wikimedia.org/wikipedia/en/2/2b/Bad_Luck_Brian.jpg",
     "https://upload.wikimedia.org/wikipedia/en/2/2b/Bad_Luck_Brian.jpg",
-   "https://static.wikia.nocookie.net/marvel_dc/images/4/4b/Batman_Vol_3_86_Textless.jpg/revision/latest/scale-to-width-down/1000?cb=20200502132734",
-    "https://upload.wikimedia.org/wikipedia/commons/b/b9/Vishnu_Surrounded_by_his_Avatars.jpg",
+   "https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg",
     "https://upload.wikimedia.org/wikipedia/commons/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg",
     "https://upload.wikimedia.org/wikipedia/en/2/2b/Bad_Luck_Brian.jpg"
 
